3ee6e7fffa5e715d23081543063f02e1ba9171ec
Ana Caroline <acarolinelimadasilva@gmail.com>
2024-05-21 14:41:12 -0300

    atividades

--- JS/04_ESTRUTURAS_CONDICIONAIS/main.js
+++ JS/04_ESTRUTURAS_CONDICIONAIS/main.js
@@ -23,7 +23,6 @@ console.log('d é 10')
 console.log('Estou fora do if que compara string')
 
 //Estrutura condicional composta
-// Estrutura condicional composta 
 console.clear(); 
 const e = '10'; 
 if (e === 10) console.log('e é o numeral 10'); 
@@ -31,3 +30,61 @@ else {
     console.log(`Resultado da condição: ${e === 10}`); 
     console.log('e não é o numeral 10'); 
 } 
+
+//Multiplas condições
+console.clear()
+const f = 4
+const g = 11
+
+if (f > 5 || g > 10) {
+   console.log(`Condição é ${f > 5 || g > 10}`)
+   console.log(`f é maior do que 5 ou g é maior que 10`)
+} else console.log(`f não é maior do que 5 e g não é maior que 10`)
+
+if (f > 5 && g > 10) console.log('f não é maior do que 5 e g é maior que 10')
+else {
+   console.log(`Condição é ${f > 5 && g > 10}`)
+   console.log('f não é maior do que 5 ou g não é maior que 10')
+}
+
+//Desvios encadeados
+console.clear()
+const num = 11
+
+if (num % 2 == 0 && !isNaN(num)) console.log('Número par!')
+else if (num % 2 !=0 && !isNaN(num)) console.log('Número ímpar')
+else console.log('Não é um número!')
+
+//Operador ternário (enxuta o código)
+console.clear()
+let resultado = 3 > 4 ? "Sim" : "Não"
+console.log(resultado)
+
+//Operador ternário - exemplo 2
+console.clear()
+let result = Math.PI < 4 ?
+console.log('Sim') : console.log('Não')
+
+Math.PI;      // retorna 3.141592653589793 
+Math.E        // retorna o número de Euler 
+Math.SQRT2    // retorna a raiz quadrada de 2 
+Math.SQRT1_2  // retorna a raiz quadrada de 1/2 
+Math.LN2      // retorna o logaritmo natural de 2 
+Math.LN10     // retorna o logaritmo natural de 10
+
+//Switch 
+const h = 11
+const cor = h > 10 ? 'vermelha' : 'azul'
+
+console.clear()
+switch (cor) {
+   case 'vermelha':
+      console.log('A cor é vermelha')
+      break
+   case 'azul':
+      console.log('A cor é azul')
+      break
+   default:
+      console.log('A cor não é vermelha ou azul')
+      break
+}
